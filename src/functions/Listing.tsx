806e5243a9def892d65d921f810d769362f99816
Spruce Campbell <spruce@mit.edu>
2023-05-02 20:53:27 -0400

    Create Next App

--- src/functions/Listing.tsx
+++
@@ -1,9 +0,0 @@
-
-export class Listing {
-    location: string = '';
-    expiry: number = 0;
-    price: string = '';
-    swipes: string = '';
-    uid: string = '';
-    nickname: string = ''
-}
--- src/functions/User.tsx
+++
@@ -1,8 +0,0 @@
-export class User {
-    uid: string = ''
-    firstName: string = ''
-    lastName: string = ''
-    venmo: string = ''
-    quote: string = ''
-    victoryQuote: string = ''
-  }
